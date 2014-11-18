1ec1229c664dc70bd4006c08c77dda3d4f5eb546
Ryan Thompson <service@aiwebsystems.com>
2014-11-18 17:48:14 -0600

    Progress

--- src/StateFieldType.php
+++ src/StateFieldType.php
@@ -2,77 +2,145 @@
 
 use Anomaly\Streams\Platform\Addon\FieldType\FieldType;
 
+/**
+ * Class StateFieldType
+ *
+ * @link          http://anomaly.is/streams-platform
+ * @author        AnomalyLabs, Inc. <hello@anomaly.is>
+ * @author        Ryan Thompson <ryan@anomaly.is>
+ * @package       Anomaly\Streams\Addon\FieldType\State
+ */
 class StateFieldType extends FieldType
 {
-    public $settings = array(
-        'countries',
-    );
 
-    public function input()
+    /**
+     * The input class.
+     *
+     * @var null
+     */
+    protected $class = null;
+
+    /**
+     * The input view.
+     *
+     * @var string
+     */
+    protected $inputView = 'field_type.state::input';
+
+    /**
+     * Get the view data for the input view.
+     *
+     * @return array
+     */
+    public function getInputData()
     {
-        return \Form::select(
-            $this->inputName(),
-            $this->states(),
-            $this->value
-        );
+        $data = parent::getInputData();
+
+        $data['options'] = $this->getOptions();
+
+        return $data;
     }
 
-    protected function states()
+    /**
+     * Return options available.
+     *
+     * @return array
+     */
+    public function getOptions()
     {
-        return array(
-            'United States' => array(
-                'AL' => 'Alabama',
-                'AK' => 'Alaska',
-                'AZ' => 'Arizona',
-                'AR' => 'Arkansas',
-                'CA' => 'California',
-                'CO' => 'Colorado',
-                'CT' => 'Connecticut',
-                'DE' => 'Deleware',
-                'DC' => 'District of Columbia',
-                'FL' => 'Florida',
-                'GA' => 'Georgia',
-                'HI' => 'Hawaii',
-                'ID' => 'Idaho',
-                'IL' => 'Illinois',
-                'IN' => 'Indiana',
-                'IA' => 'Iowa',
-                'KS' => 'Kansas',
-                'KY' => 'Kentucky',
-                'LA' => 'Louisiana',
-                'ME' => 'Maine',
-                'MD' => 'Maryland',
-                'MA' => 'Massachusetts',
-                'MI' => 'Michigan',
-                'MN' => 'Minnesota',
-                'MS' => 'Mississippi',
-                'MO' => 'Missouri',
-                'MT' => 'Montana',
-                'NE' => 'Nebraska',
-                'NV' => 'Nevada',
-                'NH' => 'New Hampshire',
-                'NJ' => 'New Jersey',
-                'NM' => 'New Mexico',
-                'NY' => 'New York',
-                'NC' => 'North Carolina',
-                'ND' => 'North Dakota',
-                'OH' => 'Ohio',
-                'OK' => 'Oklahoma',
-                'OR' => 'Oregon',
-                'PA' => 'Pennsylvania',
-                'RI' => 'Rhode Island',
-                'SC' => 'South Carolina',
-                'SD' => 'South Dakota',
-                'TN' => 'Tennessee',
-                'TX' => 'Texas',
-                'UT' => 'Utah',
-                'VT' => 'Vermont',
-                'VA' => 'Virginia',
-                'WA' => 'Washington',
-                'WV' => 'West Virginia',
-                'WI' => 'Wisconsin',
-                'WY' => 'Wyoming',
-            ),
-        );
+        $options = [];
+
+        $country = $this->getConfig('country');
+
+        foreach ($this->getStates($country) as $value => &$title) {
+
+            $options[$value] = [
+                'value'    => $value,
+                'state'    => trans($title),
+                'selected' => ($value == $this->getValue()),
+            ];
+        }
+
+        return $options;
+    }
+
+    /**
+     * Get states.
+     *
+     * @param null $country
+     * @return array
+     */
+    public function getStates($country = null)
+    {
+        $countries = [
+            'US' => [
+                'country' => 'United States',
+                'states'  => [
+                    'AL' => 'Alabama',
+                    'AK' => 'Alaska',
+                    'AZ' => 'Arizona',
+                    'AR' => 'Arkansas',
+                    'CA' => 'California',
+                    'CO' => 'Colorado',
+                    'CT' => 'Connecticut',
+                    'DE' => 'Deleware',
+                    'DC' => 'District of Columbia',
+                    'FL' => 'Florida',
+                    'GA' => 'Georgia',
+                    'HI' => 'Hawaii',
+                    'ID' => 'Idaho',
+                    'IL' => 'Illinois',
+                    'IN' => 'Indiana',
+                    'IA' => 'Iowa',
+                    'KS' => 'Kansas',
+                    'KY' => 'Kentucky',
+                    'LA' => 'Louisiana',
+                    'ME' => 'Maine',
+                    'MD' => 'Maryland',
+                    'MA' => 'Massachusetts',
+                    'MI' => 'Michigan',
+                    'MN' => 'Minnesota',
+                    'MS' => 'Mississippi',
+                    'MO' => 'Missouri',
+                    'MT' => 'Montana',
+                    'NE' => 'Nebraska',
+                    'NV' => 'Nevada',
+                    'NH' => 'New Hampshire',
+                    'NJ' => 'New Jersey',
+                    'NM' => 'New Mexico',
+                    'NY' => 'New York',
+                    'NC' => 'North Carolina',
+                    'ND' => 'North Dakota',
+                    'OH' => 'Ohio',
+                    'OK' => 'Oklahoma',
+                    'OR' => 'Oregon',
+                    'PA' => 'Pennsylvania',
+                    'RI' => 'Rhode Island',
+                    'SC' => 'South Carolina',
+                    'SD' => 'South Dakota',
+                    'TN' => 'Tennessee',
+                    'TX' => 'Texas',
+                    'UT' => 'Utah',
+                    'VT' => 'Vermont',
+                    'VA' => 'Virginia',
+                    'WA' => 'Washington',
+                    'WV' => 'West Virginia',
+                    'WI' => 'Wisconsin',
+                    'WY' => 'Wyoming',
+                ],
+            ],
+        ];
+
+        if ($country) {
+
+            if (!isset($countries[$country]['states'])) {
+
+                return [];
+            }
+
+            return $countries[$country]['states'];
+        }
+
+        return $countries;
     }
 }
